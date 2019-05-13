9da01e29700b3adf54f00d961e72f51d8faa5024
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-13 02:05:16 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
     @backed_projects = []
   end
   
-  def backed_project(Project)
-    @back_projects << Project
+  def backed_project(project)
+    @back_projects << project
   end
 end
